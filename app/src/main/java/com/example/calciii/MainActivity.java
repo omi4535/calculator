73470e2cb50241d72c15d84b5e7198cf929bc636
omkar dhage <omkardhage880500@gmail.com>
2022-02-15 00:33:44 +0530

    calculator 1st app

--- app/src/main/java/com/example/calciii/MainActivity.java
+++ app/src/main/java/com/example/calciii/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.calciii;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.BoringLayout;
 import android.view.View;
 import android.widget.EditText;
 
+
+
+
 public class MainActivity extends AppCompatActivity {
+
     EditText edt1;
     EditText edt2;
     String op="+";
@@ -19,6 +26,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Define ActionBar object
+
+
+        // Define ColorDrawable object and parse color
+        // using parseColor method
+        // with color hash code as its parameter
+        ColorDrawable colorDrawable
+                = new ColorDrawable(Color.parseColor("#FFFFFF"));
+
+        // Set BackgroundDrawable
+
         setContentView(R.layout.activity_main);
         edt1=findViewById(R.id.editTextText);
         edt2=findViewById(R.id.editTextText1);
